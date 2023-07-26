2a4b2e62fc3afd4778dc83dc64c62bbfcfe3b142
Omar Abo zaid <120683753+OomRr@users.noreply.github.com>
2023-07-26 14:27:50 +0300

    create cubit

--- lib/Features/home/presentation/views/widgets/book_details_app_bar.dart
+++ lib/Features/home/presentation/views/widgets/book_details_app_bar.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BookDetailsAppBar extends StatelessWidget {
   const BookDetailsAppBar({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        IconButton(onPressed: () {}, icon: const Icon(Icons.clear_outlined)),
+        IconButton(
+            onPressed: ()
+            {
+              GoRouter.of(context).pop();
+            },
+            icon: const Icon(Icons.clear_outlined)),
         IconButton(
             onPressed: () {}, icon: const Icon(Icons.shopping_cart_outlined)),
       ],
--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,2 +1,58 @@
-abstract class Failure{}
-class ServerFailure extends Failure{}
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  final String errorMessage;
+
+  Failure(this.errorMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errorMessage);
+
+  factory ServerFailure.fromDioError(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('timeout');
+
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('timeout');
+
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('timeout');
+
+      case DioExceptionType.badCertificate:
+        return ServerFailure('bad certificate');
+
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+            dioException.response!.statusCode!, dioException.response!.data);
+
+      case DioExceptionType.cancel:
+        return ServerFailure('request is canceled , try again');
+
+      case DioExceptionType.connectionError:
+        return ServerFailure('connection error');
+
+      case DioExceptionType.unknown:
+        if(dioException.message!.contains('SocketException'))
+          {
+            return ServerFailure('No Internet Connection');
+          }
+        return ServerFailure('unexpected error, try again');
+        default:
+          return ServerFailure('unknown error, try again');
+    }
+  }
+
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 401 || statusCode == 402 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure('page not found');
+    } else if (statusCode == 500) {
+      return ServerFailure('Server Error, try later');
+    } else {
+      return ServerFailure('unknown error');
+    }
+  }
+}
